c7327f8057d4147bafdd365fc2496f5c1dbfb5a7
saurav <sauravsingh25625@gmail.com>
2021-11-11 21:59:08 +0530

    basic

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class Numbers {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner a = new Scanner(System.in);
+        System.out.print("number is :");
+        int s = a.nextInt();
+        System.out.println("you entered :"+a);
+    }
+}
---
+++ src/style.css
@@ -0,0 +1,8 @@
+#flipkart_url, #amazon_url{
+    font-size: 30px;
+    font-family: 'Segoe UI';
+}
+
+a{
+    text-decoration: none;
+}
